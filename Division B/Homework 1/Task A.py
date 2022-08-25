95b9174873349e1b6b7c768f97a822cbe5c8770c
Valeriia Kuka <71337024+kavaivaleri@users.noreply.github.com>
2022-08-25 17:02:20 +0300

    Create Task A.py

---
+++ Division B/Homework 1/Task A.py
@@ -0,0 +1,24 @@
+r = int(input()) #целое число, код завершения задачи
+i = int(input()) #целое число, вердикт интерактора
+c = int(input()) #целое число, вердикт чекера
+
+if i == 0:
+    if r != 0:
+        result = 3
+    else:
+        result = c
+elif i == 1:
+    result = c
+elif i == 4:
+    if r != 0:
+        result = 3
+    else:
+        result = 4
+elif i == 6:
+    result = 0
+elif i == 7:
+    result = 1
+else:
+    result = i
+
+print(result)
